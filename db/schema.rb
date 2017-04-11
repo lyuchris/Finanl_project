671ba1349a76771283eb855aa9cf261a41a28618
Chris <tw.chrislyu@gmail.com>
2017-04-11 13:35:46 +0800

    products_show_jbuilder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409084605) do
+ActiveRecord::Schema.define(version: 20170411022414) do
 
   create_table "donations", force: :cascade do |t|
     t.integer  "product_id"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 20170409084605) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "role"
+    t.string   "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
